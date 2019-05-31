cf9d4fc106436caa71a53e79b95297a3ec880280
John Cross <jwcross1@dmacc.edu>
2019-05-31 13:34:43 -0500

    first commit

---
+++ src/com/johncross/Main.java
@@ -0,0 +1,84 @@
+package com.johncross;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 2 operators; = and +
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+
+        result = result - 1;
+        System.out.println(previousResult + " - 1 = " + result);
+
+        previousResult = result;
+
+        result = result * 10;
+        System.out.println(previousResult + " * 10 =  " + result);
+
+        previousResult = result;
+
+        result = result / 5;
+        System.out.println(previousResult + " / 5 =  " + result);
+
+        previousResult = result;
+        result = result % 3;
+        System.out.println(previousResult + " % 3 =  " + result);
+
+        previousResult = result;
+        result = result + 1;
+        System.out.println("Result is now: " + result);
+        result++;
+        System.out.println("Result is now " + result);
+        result--;
+        System.out.println("Result is now " + result);
+
+        result += 2;
+        System.out.println("Result is now " + result);
+        result *= 10;
+        System.out.println("Result is now " + result);
+        result -= 10;
+        System.out.println("Result is now " + result);
+        result /= 10;
+        System.out.println("Result is now " + result);
+
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("It is not an alien!");
+
+        int topScore = 80;
+        if (topScore < 100)
+            System.out.println("You got the high score!");
+
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100))
+            System.out.println("Greater than second top score and less than 100");
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true");
+
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is true");
+
+        boolean  isCar = false;
+        if (isCar)
+            System.out.println("This is not supposed to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
+        double myVal = 20d;
+        double myOtherVal = 80d;
+        double myAnswer = (myVal + myOtherVal) * 25;
+        System.out.println("myAnswer = " + myAnswer);
+        double remainder = myAnswer % 40;
+        System.out.println("Remainder is " + remainder);
+        if (remainder <= 20)
+            System.out.println("Total was over the limit");
+
+    }
+}
